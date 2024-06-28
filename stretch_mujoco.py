67709e09add68d0c2435c3d104e72ec0fad62b03
Mohamed Fazil <mfazil@hello-robot.com>
2024-06-28 00:08:37 -0700

    Add gamepad controller

--- stretch_mujoco.py
+++ stretch_mujoco.py
@@ -1,19 +1,22 @@
 """
 Python sample script for interfacing with the Stretch Mujoco simulator
 """
+
 import time
 import threading
 import mujoco
 import mujoco.viewer
 from mujoco import MjModel, MjData
 import cv2
+import numpy as np
+
 
 class StretchMujocoSimulator:
     """
     StretchMujocoSimulator sample class for simulating Stretch robot in Mujoco
     """
-    def __init__(self, 
-                 scene_xml_path: str = "./scene.xml"):
+
+    def __init__(self, scene_xml_path: str = "./scene.xml"):
         self.mjmodel = mujoco.MjModel.from_xml_path(scene_xml_path)
         self.mjdata = mujoco.MjData(self.mjmodel)
 
@@ -22,169 +25,179 @@ class StretchMujocoSimulator:
         self.depth_renderer.enable_depth_rendering()
         self.wheel_diameter = 0.1016
         self.wheel_seperation = 0.3153
-        self.status = {'time': None, 
-                       'base': {'x_vel': None,'theta_vel': None},
-                       'lift': {'pos': None,'vel': None},
-                       'arm': {'pos': None,'vel': None},
-                       'head_pan': {'pos': None,'vel': None},
-                       'head_tilt': {'pos': None,'vel': None},
-                       'wrist_yaw': {'pos': None,'vel': None},
-                       'wrist_pitch': {'pos': None,'vel': None},
-                       'wrist_roll': {'pos': None,'vel': None},
-                       'gripper': {'pos': None,'vel': None},
-                      }
-    
-    def home(self)->None:
-        """
-        Move the robot to home position"""
-        self.mjdata.ctrl = self.mjmodel.keyframe('home').ctrl
+        self.status = {
+            "time": None,
+            "base": {"x_vel": None, "theta_vel": None},
+            "lift": {"pos": None, "vel": None},
+            "arm": {"pos": None, "vel": None},
+            "head_pan": {"pos": None, "vel": None},
+            "head_tilt": {"pos": None, "vel": None},
+            "wrist_yaw": {"pos": None, "vel": None},
+            "wrist_pitch": {"pos": None, "vel": None},
+            "wrist_roll": {"pos": None, "vel": None},
+            "gripper": {"pos": None, "vel": None},
+        }
 
-    def stow(self)->None:
+    def home(self) -> None:
         """
-        Move the robot to stow position"""
-        self.mjdata.ctrl = self.mjmodel.keyframe('stow').ctrl
+        Move the robot to home position
+        """
+        self.mjdata.ctrl = self.mjmodel.keyframe("home").ctrl
 
-    def move_to(self,
-                actuator_name:str,
-                pos:float)->None:
+    def stow(self) -> None:
         """
-        Move the actuator to a specific position"""
+        Move the robot to stow position
+        """
+        self.mjdata.ctrl = self.mjmodel.keyframe("stow").ctrl
+
+    def move_to(self, actuator_name: str, pos: float) -> None:
+        """
+        Move the actuator to a specific position
+        """
         self.mjdata.actuator(actuator_name).ctrl = pos
 
-    def move_by(self, 
-                actuator_name:str, 
-                pos:float)->None:
+    def move_by(self, actuator_name: str, pos: float) -> None:
         """
-        Move the actuator by a specific amount"""
-        self.mjdata.actuator(actuator_name).ctrl = self.status[actuator_name]['pos'] + pos
-    
-    def set_base_velocity(self,
-                          v_linear:float,
-                          omega:float)->None:
+        Move the actuator by a specific amount
         """
-        Set the base velocity of the robot"""
+        self.mjdata.actuator(actuator_name).ctrl = self.status[actuator_name]["pos"] + pos
+
+    def set_base_velocity(self, v_linear: float, omega: float) -> None:
+        """
+        Set the base velocity of the robot
+        """
         w_left, w_right = self.diff_drive_inv_kinematics(v_linear, omega)
         self.mjdata.actuator("left_wheel_vel").ctrl = w_left
         self.mjdata.actuator("right_wheel_vel").ctrl = w_right
 
-
-    def set_velocity(self, 
-                     actuator_name:str, 
-                     vel:float)->None:
+    def set_velocity(self, actuator_name: str, vel: float) -> None:
         """
-        Set the velocity of the actuator"""
-        # TODO: Implement this method by ether moving to an integrated velocity acuators or have seperate 
-        #       robot xml configured by replacing position with velocity ctrl actuators
+        Set the velocity of the actuator
+        """
+        # TODO: Implement this method by ether moving to an integrated velocity acuators or have
+        # seperate robot xml configured by replacing position with velocity ctrl actuators
         raise NotImplementedError
-    
-    def pull_status(self)->None:
+
+    def pull_status(self) -> None:
         """
-        Pull joints status of the robot from the simulator"""
-        self.status['time'] = self.mjdata.time
-        self.status['lift']['pos'] = self.mjdata.actuator('lift').length[0]
-        self.status['lift']['vel'] = self.mjdata.actuator('lift').velocity[0]
-
-        self.status['arm']['pos'] = self.mjdata.actuator('arm').length[0]
-        self.status['arm']['vel'] = self.mjdata.actuator('arm').velocity[0]
-
-        self.status['head_pan']['pos'] = self.mjdata.actuator('head_pan').length[0]
-        self.status['head_pan']['vel'] = self.mjdata.actuator('head_pan').velocity[0]
-
-        self.status['head_tilt']['pos'] = self.mjdata.actuator('head_tilt').length[0]
-        self.status['head_tilt']['vel'] = self.mjdata.actuator('head_tilt').velocity[0]
-
-        self.status['wrist_yaw']['pos'] = self.mjdata.actuator('wrist_yaw').length[0]
-        self.status['wrist_yaw']['vel'] = self.mjdata.actuator('wrist_yaw').velocity[0]
-
-        self.status['wrist_pitch']['pos'] = self.mjdata.actuator('wrist_pitch').length[0]
-        self.status['wrist_pitch']['vel'] = self.mjdata.actuator('wrist_pitch').velocity[0]
-
-        self.status['wrist_roll']['pos'] = self.mjdata.actuator('wrist_roll').length[0]
-        self.status['wrist_roll']['vel'] = self.mjdata.actuator('wrist_roll').velocity[0]
-
-        self.status['gripper']['pos'] = self.mjdata.actuator('gripper').length[0]
-        self.status['gripper']['vel'] = self.mjdata.actuator('gripper').velocity[0]
-
-        left_wheel_vel = self.mjdata.actuator('left_wheel_vel').velocity[0]
-        right_wheel_vel = self.mjdata.actuator('right_wheel_vel').velocity[0]
-        self.status['base']['x_vel'], self.status['base']['theta_vel'] = self.diff_drive_fwd_kinematics(left_wheel_vel, right_wheel_vel)
-    
-    def pull_camera_data(self)->dict:
+        Pull joints status of the robot from the simulator
         """
-        Pull camera data from the simulator"""
+        self.status["time"] = self.mjdata.time
+        self.status["lift"]["pos"] = self.mjdata.actuator("lift").length[0]
+        self.status["lift"]["vel"] = self.mjdata.actuator("lift").velocity[0]
+
+        self.status["arm"]["pos"] = self.mjdata.actuator("arm").length[0]
+        self.status["arm"]["vel"] = self.mjdata.actuator("arm").velocity[0]
+
+        self.status["head_pan"]["pos"] = self.mjdata.actuator("head_pan").length[0]
+        self.status["head_pan"]["vel"] = self.mjdata.actuator("head_pan").velocity[0]
+
+        self.status["head_tilt"]["pos"] = self.mjdata.actuator("head_tilt").length[0]
+        self.status["head_tilt"]["vel"] = self.mjdata.actuator("head_tilt").velocity[0]
+
+        self.status["wrist_yaw"]["pos"] = self.mjdata.actuator("wrist_yaw").length[0]
+        self.status["wrist_yaw"]["vel"] = self.mjdata.actuator("wrist_yaw").velocity[0]
+
+        self.status["wrist_pitch"]["pos"] = self.mjdata.actuator("wrist_pitch").length[0]
+        self.status["wrist_pitch"]["vel"] = self.mjdata.actuator("wrist_pitch").velocity[0]
+
+        self.status["wrist_roll"]["pos"] = self.mjdata.actuator("wrist_roll").length[0]
+        self.status["wrist_roll"]["vel"] = self.mjdata.actuator("wrist_roll").velocity[0]
+
+        self.status["gripper"]["pos"] = self.mjdata.actuator("gripper").length[0]
+        self.status["gripper"]["vel"] = self.mjdata.actuator("gripper").velocity[0]
+
+        left_wheel_vel = self.mjdata.actuator("left_wheel_vel").velocity[0]
+        right_wheel_vel = self.mjdata.actuator("right_wheel_vel").velocity[0]
+        self.status["base"]["x_vel"], self.status["base"]["theta_vel"] = (
+            self.diff_drive_fwd_kinematics(left_wheel_vel, right_wheel_vel)
+        )
+
+    def pull_camera_data(self) -> dict:
+        """
+        Pull camera data from the simulator
+        """
         data = {}
-        data['time'] = self.mjdata.time
-        self.rgb_renderer.update_scene(self.mjdata,'d405_rgb')
-        self.depth_renderer.update_scene(self.mjdata,'d405_rgb')
-        data['cam_d405_rgb'] = cv2.cvtColor(self.rgb_renderer.render(), cv2.COLOR_RGB2BGR)
-        data['cam_d405_depth'] = self.depth_renderer.render()
+        data["time"] = self.mjdata.time
+        self.rgb_renderer.update_scene(self.mjdata, "d405_rgb")
+        self.depth_renderer.update_scene(self.mjdata, "d405_rgb")
+        data["cam_d405_rgb"] = cv2.cvtColor(self.rgb_renderer.render(), cv2.COLOR_RGB2BGR)
+        data["cam_d405_depth"] = self.depth_renderer.render()
 
-        self.rgb_renderer.update_scene(self.mjdata,'d435i_camera_rgb')
-        self.depth_renderer.update_scene(self.mjdata,'d435i_camera_rgb')
-        data['cam_d435i_rgb'] = cv2.rotate(cv2.cvtColor(self.rgb_renderer.render(), cv2.COLOR_RGB2BGR), cv2.ROTATE_90_COUNTERCLOCKWISE)
-        data['cam_d435i_depth'] = cv2.rotate(self.depth_renderer.render(), cv2.ROTATE_90_COUNTERCLOCKWISE)
+        self.rgb_renderer.update_scene(self.mjdata, "d435i_camera_rgb")
+        self.depth_renderer.update_scene(self.mjdata, "d435i_camera_rgb")
+        data["cam_d435i_rgb"] = cv2.rotate(
+            cv2.cvtColor(self.rgb_renderer.render(), cv2.COLOR_RGB2BGR),
+            cv2.ROTATE_90_COUNTERCLOCKWISE,
+        )
+        data["cam_d435i_depth"] = cv2.rotate(
+            self.depth_renderer.render(), cv2.ROTATE_90_COUNTERCLOCKWISE
+        )
 
-        self.rgb_renderer.update_scene(self.mjdata,'nav_camera_rgb')
-        data['cam_nav_rgb'] = cv2.cvtColor(self.rgb_renderer.render(), cv2.COLOR_RGB2BGR)
+        self.rgb_renderer.update_scene(self.mjdata, "nav_camera_rgb")
+        data["cam_nav_rgb"] = cv2.cvtColor(self.rgb_renderer.render(), cv2.COLOR_RGB2BGR)
         return data
 
-    def __ctrl_callback(self, model: MjModel, data: MjData)->None:
+    def __ctrl_callback(self, model: MjModel, data: MjData) -> None:
         """
-        Callback function that gets executed with mj_step"""
+        Callback function that gets executed with mj_step
+        """
         self.mjdata = data
         self.mjmodel = model
         self.pull_status()
 
-    def diff_drive_inv_kinematics(self,
-                                      V:float,
-                                      omega:float)->tuple:
+    def diff_drive_inv_kinematics(self, V: float, omega: float) -> tuple:
         """
-        Calculate the rotational velocities of the left and right wheels for a differential drive robot."""
+        Calculate the rotational velocities of the left and right wheels for a
+        differential drive robot.
+        """
         R = self.wheel_diameter / 2
         L = self.wheel_seperation
         if R <= 0:
             raise ValueError("Radius must be greater than zero.")
         if L <= 0:
             raise ValueError("Distance between wheels must be greater than zero.")
-        
+
         # Calculate the rotational velocities of the wheels
         w_left = (V - (omega * L / 2)) / R
         w_right = (V + (omega * L / 2)) / R
-        
+
         return (w_left, w_right)
 
-    def diff_drive_fwd_kinematics(self,
-                              w_left:float,
-                              w_right:float)->tuple:
+    def diff_drive_fwd_kinematics(self, w_left: float, w_right: float) -> tuple:
         """
-        Calculate the linear and angular velocity of a differential drive robot."""
+        Calculate the linear and angular velocity of a differential drive robot.
+        """
         R = self.wheel_diameter / 2
         L = self.wheel_seperation
         if R <= 0:
             raise ValueError("Radius must be greater than zero.")
         if L <= 0:
             raise ValueError("Distance between wheels must be greater than zero.")
-        
+
         # Linear velocity (V) is the average of the linear velocities of the two wheels
         V = R * (w_left + w_right) / 2.0
-        
-        # Angular velocity (omega) is the difference in linear velocities divided by the distance between the wheels
+
+        # Angular velocity (omega) is the difference in linear velocities divided by the distance
+        # between the wheels
         omega = R * (w_right - w_left) / L
-        
+
         return (V, omega)
 
-    def __run(self)->None:
+    def __run(self) -> None:
         mujoco.set_mjcb_control(self.__ctrl_callback)
         mujoco.viewer.launch(self.mjmodel)
 
-    def start(self)->None:
+    def start(self) -> None:
         """
-        Start the simulator in a using blocking Managed-vieiwer for precise timing. And user code is looped through callback.
-        For some projects a non-blocking Passive-vieiwer might be more suitable. For more info visit:
-        https://mujoco.readthedocs.io/en/stable/python.html#managed-viewer"""
+        Start the simulator in a using blocking Managed-vieiwer for precise timing. And user code
+        is looped through callback. Some projects might need non-blocking Passive-vieiwer.
+        For more info visit: https://mujoco.readthedocs.io/en/stable/python.html#managed-viewer
+        """
         threading.Thread(target=self.__run).start()
         time.sleep(0.5)
         self.home()
+
 
 if __name__ == "__main__":
     robot_sim = StretchMujocoSimulator()
@@ -192,11 +205,10 @@ if __name__ == "__main__":
     # display camera feeds
     while True:
         camera_data = robot_sim.pull_camera_data()
-        cv2.imshow('cam_d405_rgb', camera_data['cam_d405_rgb'])
-        cv2.imshow('cam_d405_depth', camera_data['cam_d405_depth'])
-        cv2.imshow('cam_d435i_rgb', camera_data['cam_d435i_rgb'])
-        cv2.imshow('cam_d435i_depth', camera_data['cam_d435i_depth'])
-        cv2.imshow('cam_nav_rgb', camera_data['cam_nav_rgb'])
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow("cam_d405_rgb", camera_data["cam_d405_rgb"])
+        cv2.imshow("cam_d405_depth", camera_data["cam_d405_depth"])
+        cv2.imshow("cam_d435i_rgb", camera_data["cam_d435i_rgb"])
+        cv2.imshow("cam_d435i_depth", camera_data["cam_d435i_depth"])
+        cv2.imshow("cam_nav_rgb", camera_data["cam_nav_rgb"])
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-        
--- stretch_mujoco_gamepad.py
+++ stretch_mujoco_gamepad.py
@@ -1,5 +1,5 @@
 from stretch_mujoco import StretchMujocoSimulator
-from stretch_body.gamepad_controller import GamePadController
+from gamepad_controller import GamePadController
 import time
 import threading
 import cv2
@@ -12,90 +12,100 @@ def display_camera_feeds():
     # display camera feeds
     while True:
         camera_data = robot_sim.pull_camera_data()
-        cv2.imshow('cam_d405_rgb', camera_data['cam_d405_rgb'])
-        cv2.imshow('cam_d405_depth', camera_data['cam_d405_depth'])
-        cv2.imshow('cam_d435i_rgb', camera_data['cam_d435i_rgb'])
-        cv2.imshow('cam_d435i_depth', camera_data['cam_d435i_depth'])
-        cv2.imshow('cam_nav_rgb', camera_data['cam_nav_rgb'])
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow("cam_d405_rgb", camera_data["cam_d405_rgb"])
+        cv2.imshow("cam_d405_depth", camera_data["cam_d405_depth"])
+        cv2.imshow("cam_d435i_rgb", camera_data["cam_d435i_rgb"])
+        cv2.imshow("cam_d435i_depth", camera_data["cam_d435i_depth"])
+        cv2.imshow("cam_nav_rgb", camera_data["cam_nav_rgb"])
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
-button_mapping = {'top_pad_pressed':['wrist_pitch', 1, 0.05],
-                  'bottom_pad_pressed':['wrist_pitch', -1, 0.05],
-                  'left_pad_pressed':['wrist_roll', -1, 0.07],
-                  'right_pad_pressed':['wrist_roll', 1, 0.07],
-                  'right_shoulder_button_pressed':['wrist_yaw', -1, 0.2],
-                  'left_shoulder_button_pressed':['wrist_yaw', 1, 0.2],
-                  'top_button_pressed': ['stow', 0],
-                  'left_button_pressed': ['dex_switch', 0],
-                  'right_button_pressed': ['gripper',1, 0.003],
-                  'bottom_button_pressed': ['gripper',-1, 0.003]
-                  }
 
-stick_mapping = {'right_stick_x': ('arm', 'inc',0.05),
-                 'right_stick_y': ('lift', 'inc',0.15),
-                 'left_stick_x': ('turn', 'scale',1),
-                 'left_stick_y': ('forward', 'scale',1)}
+button_mapping = {
+    "top_pad_pressed": ["wrist_pitch", 1, 0.05],
+    "bottom_pad_pressed": ["wrist_pitch", -1, 0.05],
+    "left_pad_pressed": ["wrist_roll", -1, 0.07],
+    "right_pad_pressed": ["wrist_roll", 1, 0.07],
+    "right_shoulder_button_pressed": ["wrist_yaw", -1, 0.2],
+    "left_shoulder_button_pressed": ["wrist_yaw", 1, 0.2],
+    "top_button_pressed": ["stow", 0],
+    "left_button_pressed": ["dex_switch", 0],
+    "right_button_pressed": ["gripper", 1, 0.003],
+    "bottom_button_pressed": ["gripper", -1, 0.003],
+}
+
+stick_mapping = {
+    "right_stick_x": ("arm", "inc", 0.05),
+    "right_stick_y": ("lift", "inc", 0.15),
+    "left_stick_x": ("turn", "scale", 1),
+    "left_stick_y": ("forward", "scale", 1),
+}
+
 
 def map_value(value, in_min, in_max, out_min, out_max):
     return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
+
 def gamepad_loop():
     dex_switch = False
-    gripper_val = robot_sim.status['gripper']['pos']
+    gripper_val = robot_sim.status["gripper"]["pos"]
     while True:
-        time.sleep(1/15)
+        time.sleep(1 / 15)
         gamepad_state = gamepad.get_state()
         for button in button_mapping.keys():
             if gamepad_state[button]:
                 try:
-                    
+
                     actuator_name, dir, k = button_mapping[button]
-                    if actuator_name != 'gripper':
-                        pos = robot_sim.status[actuator_name]['pos'] + dir*k
+                    if actuator_name != "gripper":
+                        pos = robot_sim.status[actuator_name]["pos"] + dir * k
                         print(f"Moving {actuator_name} to {pos}")
                         robot_sim.move_to(actuator_name, pos)
-                    elif actuator_name == 'gripper':
-                        gripper_val = gripper_val + dir*k
+                    elif actuator_name == "gripper":
+                        gripper_val = gripper_val + dir * k
                         gripper_val = max(min(gripper_val, 0.04), -0.02)
                         print(f"Moving {actuator_name} to {gripper_val}")
-                        robot_sim.move_to(actuator_name, gripper_val)                        
+                        robot_sim.move_to(actuator_name, gripper_val)
 
                 except:
                     pass
-        if gamepad_state['left_button_pressed']:
+        if gamepad_state["left_button_pressed"]:
             dex_switch = not dex_switch
             print(f"Setting dex_switch to {dex_switch}")
             if dex_switch:
-                button_mapping['bottom_pad_pressed'][0] = 'head_tilt'
-                button_mapping['top_pad_pressed'][0] = 'head_tilt'
-                button_mapping['left_pad_pressed'][0] = 'head_pan'
-                button_mapping['right_pad_pressed'][0] = 'head_pan'
+                button_mapping["bottom_pad_pressed"][0] = "head_tilt"
+                button_mapping["top_pad_pressed"][0] = "head_tilt"
+                button_mapping["left_pad_pressed"][0] = "head_pan"
+                button_mapping["right_pad_pressed"][0] = "head_pan"
             else:
-                button_mapping['top_pad_pressed'][0] = 'wrist_pitch'
-                button_mapping['bottom_pad_pressed'][0] = 'wrist_pitch'
-                button_mapping['left_pad_pressed'][0] = 'wrist_roll'
-                button_mapping['right_pad_pressed'][0] = 'wrist_roll'
+                button_mapping["top_pad_pressed"][0] = "wrist_pitch"
+                button_mapping["bottom_pad_pressed"][0] = "wrist_pitch"
+                button_mapping["left_pad_pressed"][0] = "wrist_roll"
+                button_mapping["right_pad_pressed"][0] = "wrist_roll"
 
         for stick in stick_mapping.keys():
-            if abs(gamepad_state[stick])>0.001:
+            if abs(gamepad_state[stick]) > 0.001:
                 actuator_name, prop, val = stick_mapping[stick]
-                if prop == 'inc':
-                    pos = robot_sim.status[actuator_name]['pos'] + gamepad_state[stick]*val
+                if prop == "inc":
+                    pos = robot_sim.status[actuator_name]["pos"] + gamepad_state[stick] * val
                     robot_sim.move_to(actuator_name, pos)
                     print(f"Moving {actuator_name} to {pos}")
-        if abs(gamepad_state['left_stick_x']) < 0.001 and abs(gamepad_state['left_stick_y']) < 0.001:
+        if (
+            abs(gamepad_state["left_stick_x"]) < 0.001
+            and abs(gamepad_state["left_stick_y"]) < 0.001
+        ):
             robot_sim.set_base_velocity(0, 0)
         else:
-            v_linear = map_value(gamepad_state['left_stick_y'], -1, 1, -0.3, 0.3)
-            omega = -map_value(gamepad_state['left_stick_x'], -1, 1, -2, 2)
+            v_linear = map_value(gamepad_state["left_stick_y"], -1, 1, -0.3, 0.3)
+            omega = -map_value(gamepad_state["left_stick_x"], -1, 1, -2, 2)
             robot_sim.set_base_velocity(v_linear, omega)
             print(f"Setting base velocity to {v_linear, omega}")
 
-        if gamepad_state['top_button_pressed']:
+        if gamepad_state["top_button_pressed"]:
             robot_sim.stow()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     robot_sim.start()
     gamepad.start()
     threading.Thread(target=gamepad_loop).start()
