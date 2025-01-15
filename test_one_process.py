6ef8446174d0b5a0669d3f039ad937e56a16f23f
Binit Shah <bshah@hello-robot.com>
2025-01-15 00:44:44 -0500

    test_one_process.py

---
+++ test_one_process.py
@@ -0,0 +1,7 @@
+import threading
+from stretch_mujoco.stretch_mujoco import launch_server
+
+command = {'val': {}}
+status = {'val': {}}
+imagery = {'val': {}}
+launch_server(None, None, "off", False, False, threading.Event(), command, status, imagery)
