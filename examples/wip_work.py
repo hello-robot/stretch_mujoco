cef32a7b1ad9fb9f657d35d463c6128cc8568ff2
Binit Shah <bshah@hello-robot.com>
2025-06-24 15:20:33 -0400

    plot scan

--- examples/wip_work.py
+++ examples/wip_work.py
@@ -1,13 +1,29 @@
 from stretch_mujoco import StretchMujocoSimulator
 from stretch_mujoco.enums.stretch_sensors import StretchSensors
 
+import zmq
 import time
 import math
 import numpy as np
 np.set_printoptions(precision=3, linewidth=100, suppress=True)
 
-
+# Program
 prev = time.time()
+
+# Networking
+ctx = zmq.Context()
+sock = ctx.socket(zmq.PUB)
+sock.setsockopt(zmq.SNDHWM, 1)
+sock.setsockopt(zmq.RCVHWM, 1)
+sock.connect(f"tcp://127.0.0.1:8080")
+
+
+def to_cartesian(arr):
+    x = arr[:,1] * np.cos(arr[:,0])
+    y = arr[:,1] * np.sin(arr[:,0])
+    xy = np.stack((x, y), axis=1)
+    return xy
+
 
 def prepare_scan(sim):
     sensor_data = sim.pull_sensor_data()
@@ -44,7 +60,21 @@ def update(sim):
     print(f'Rate: {1/delta:.2f} Hz, Scan size: {scan.shape}')
     prev = now
 
-    print(scan.shape, scan)
+    # Polar & cartesian update
+    polar_offsets = scan
+    cartesian_offsets = to_cartesian(polar_offsets)
+
+    dock_pts = np.empty((0,2))
+    both_sides = np.empty((0,2))
+    line_points = np.empty((0,2))
+
+    sock.send_pyobj({
+        'polar_offsets': polar_offsets,
+        'cartesian_offsets': cartesian_offsets,
+        'cut_both_sides': dock_pts,
+        'both_sides': both_sides,
+        'line_points': line_points,
+    })
 
 
 if __name__ == "__main__":
