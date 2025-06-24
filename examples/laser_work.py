9a562d5a732fe3e50fd5743fb5766b50a1a1df95
Binit Shah <bshah@hello-robot.com>
2025-06-24 12:43:55 -0400

    Only 6 points?

--- examples/laser_work.py
+++ examples/laser_work.py
@@ -1,20 +1,59 @@
 from stretch_mujoco import StretchMujocoSimulator
+from stretch_mujoco.enums.stretch_sensors import StretchSensors
 
 import time
 import math
 import numpy as np
-from pprint import pprint
+np.set_printoptions(precision=3, linewidth=100, suppress=True)
+
+
+prev = time.time()
+
+def prepare_scan(sim):
+    sensor_data = sim.pull_sensor_data()
+    scan_data = sensor_data.get_data(StretchSensors.base_lidar)
+
+    lower_bound = 0.2
+    upper_bound = 5
+
+    mask_lower = scan_data >= lower_bound
+    mask_upper = scan_data <= upper_bound
+
+    filtered_distance = scan_data[mask_lower & mask_upper] * 1000
+    if len(filtered_distance) == 0:
+        return np.empty((0, 2))
+
+    degrees = np.array(range(len(scan_data)))
+    angles = np.radians(degrees)
+    angles_filtered = angles[mask_lower & mask_upper]
+
+    scan = np.stack([angles_filtered, filtered_distance], axis=1)
+    return scan
+
+
+def update(sim):
+    global prev
+
+    # Get Scan
+    scan = prepare_scan(sim)
+
+    # Log rate
+    now = time.time()
+    delta = now - prev
+    print(f'Rate: {1/delta:.2f} Hz, Scan size: {scan.shape}')
+    prev = now
+
+    print(scan.shape, scan)
 
 
 if __name__ == "__main__":
     sim = StretchMujocoSimulator()
-    sim.start(headless=True)
-
+    sim.start()
     print("Start!")
-    for _ in range(10):
-        time.sleep(1)
-        scan = sim.pull_sensor_data().lidar
-        print(scan.shape)
+
+    while True:
+        update(sim)
+        time.sleep(0.1)
 
     print('Done!')
     sim.stop()
