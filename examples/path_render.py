8352ae724099fe318bcc6307d365caed3a22a89c
Binit Shah <bshah@hello-robot.com>
2025-08-06 15:57:41 -0400

    Switch to top-down ortho view

--- examples/path_render.py
+++ examples/path_render.py
@@ -20,10 +20,20 @@ scene_xml = str(Path.cwd() / "stretch_mujoco" / "models" / "dock_pen.xml")
 server = MujocoServer(scene_xml_path=scene_xml, model=None, stop_mujoco_process_event=event, data_proxies=data_proxies)
 
 # Setup rendering
-h, w = (480, 640)
+h, w = (640, 640)
 server.mjmodel.vis.global_.offheight = h
 server.mjmodel.vis.global_.offwidth = w
 renderer = mujoco.Renderer(server.mjmodel, height=h, width=w)
+
+# Setup orthographic top-down camera
+cam = mujoco.MjvCamera()
+mujoco.mjv_defaultCamera(cam) # inits cam with defaults
+cam.type = mujoco.mjtCamera.mjCAMERA_FREE
+cam.orthographic = True
+cam.azimuth = 0
+cam.elevation = -90 # look straight down
+cam.lookat[:] = server.mjmodel.stat.center
+cam.distance = 3.5*server.mjmodel.stat.extent  # zoom so whole scene fits
 
 # Set to home keyframe
 for i in range(server.mjmodel.nkey):
@@ -36,6 +46,6 @@ while server.mjdata.time < 1.0:
 
 # Render scene
 mujoco.mj_forward(server.mjmodel, server.mjdata)
-renderer.update_scene(server.mjdata)
+renderer.update_scene(server.mjdata, camera=cam)
 img = renderer.render()
 cv2.imwrite('test.png', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
